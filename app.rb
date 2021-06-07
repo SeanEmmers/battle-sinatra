dfc444372c121f922df5927accf12ff3f8404526
Sean Phillips <seanphillips@Seans-Air.broadband>
2021-06-07 16:40:09 +0100

    Adding first HTML image

--- app.rb
+++ app.rb
@@ -1,10 +1,16 @@
 require 'sinatra'
-# require "sinatra/reloader" if development?
+require "sinatra/reloader" if development?
 
 get '/' do
   "Hello"
 end
 
 get '/secret' do
-  'This is a secret page'
+  'This is not a secret page'
+end
+
+get '/cat' do
+  "<div>
+    <img src='https://i.imgur.com/q1jGYH6.jpg'>
+   </div>"
 end
